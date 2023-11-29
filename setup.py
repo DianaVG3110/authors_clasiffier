877374e146b0ac4b75524f3d7263c0ec65124e3a
Iván García <77176510+ivanGarciaBernal@users.noreply.github.com>
2023-11-29 15:56:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='minimal',
+setup(name='authors_clasiffier',
       version='0.1',
-      description='A Python package for something',
-      url='http://github.com/rvf0068/minimal',
-      author='Yo Mero',
-      author_email='yomero@gmail.com',
+      description='A Python package for classify words into authors',
+      url='https://github.com/DianaVG3110/authors_classiffier',
+      author='Diana Vera, Ana Olmedo, Luis Ríos, Iván García',
+      author_email='ve333269@uaeh.edu.mx, ol333736@uaeh.edu.mx, ri353856@uaeh.edu.mx, ga419623@uaeh.edu.mx',
       license='MIT',
       packages=['minimal'],
       zip_safe=False)
