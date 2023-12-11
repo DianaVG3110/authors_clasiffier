67b2d543899cf9e71b98e8ee20d31a295f7c334e
Iván García <77176510+ivanGarciaBernal@users.noreply.github.com>
2023-12-10 21:46:20 -0600

    add dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@
 
 from setuptools import setup
 
-setup(name='authors_clasiffier',
-      version='0.1',
-      description='A Python package for classify words into authors',
-      url='https://github.com/DianaVG3110/authors_classiffier',
-      author='Diana Vera, Ana Olmedo, Luis Ríos, Iván García',
-      author_email='ve333269@uaeh.edu.mx, ol333736@uaeh.edu.mx, ri353856@uaeh.edu.mx, ga419623@uaeh.edu.mx',
-      license='MIT',
-      packages=['minimal'],
-      zip_safe=False)
+setup(
+    name="authors_clasiffier",
+    version="0.1",
+    description="A Python package for classify words into authors",
+    url="https://github.com/DianaVG3110/authors_classiffier",
+    author="Diana Vera, Ana Olmedo, Luis Ríos, Iván García",
+    author_email="ve333269@uaeh.edu.mx, \
+      ol333736@uaeh.edu.mx, ri353856@uaeh.edu.mx, ga419623@uaeh.edu.mx",
+    license="MIT",
+    packages=["minimal"],
+    zip_safe=False,
+    install_requires=[
+        "wordcloud"
+    ]
+)
