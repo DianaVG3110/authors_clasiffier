8924d60de6564f81134323068c51679cb20a07d6
Iván García <77176510+ivanGarciaBernal@users.noreply.github.com>
2023-12-10 20:34:06 -0600

    fix path handling in Windows

--- authors_clasiffier/word_cloud.py
+++ authors_clasiffier/word_cloud.py
@@ -1,24 +1,28 @@
 """This file creates wordclouds using text files"""
 
 from wordcloud import WordCloud
-from pathlib import Path
 import os
 
 def gen_wordcloud(file, output):
-    """Generates wordcloud from `file` and saves at `output`"""
-    text = open(file).read()
+    """Generates wordcloud from `file` and saves at `output.jpg`"""
+    try:
+        text = open(file).read()
+    except:
+        text = open(file, encoding="cp850").read()
     wordcloud = WordCloud(width = 1280 , height = 720).generate(text)
-    wordcloud.to_file(Path(str(output) +  '.jpg'))
+    wordcloud.to_file(output)
 
 def truncate_filename(filename):
-    return(Path(filename).stem)
+    return os.path.splitext(filename)
 
-def gen_wordcloud_directory(path):
-    p = Path(path)
-    output_dir = Path("../images/")
+def gen_wordcloud_directory(dirname):
+    dir = os.path.dirname(__file__)
+    path = os.path.join(dir,dirname)
+    output_dir = os.path.join(dir, "../images/")
     for filename in os.listdir(path):
-        f = Path(filename)
-        path_ = p / f
-        print(path_)
-        gen_wordcloud(path_,
-                      output_dir / Path(truncate_filename(filename)))
+        file = os.path.join(path,filename)
+        gen_wordcloud(file,
+                      os.path.join(output_dir,truncate_filename(filename)[0] + '.jpg'))
+
+if __name__ == "__main__":
+    gen_wordcloud_directory("../data")
