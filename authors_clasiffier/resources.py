edb0b6a2ad761791ba514ddc6bd6c4549806078c
LuisHRiosC18 <141874238+LuisHRiosC18@users.noreply.github.com>
2024-01-09 17:31:10 -0600

    Update and rename primer_archivo.py to resources.py

---
+++ authors_clasiffier/resources.py
@@ -0,0 +1,12 @@
+"""test resources"""
+
+
+import pkg_resources
+
+ae_1= pkg_resources.resource_filename('authors_clasiffier', '/data/AlejandroSanz.txt') 
+ae_2= pkg_resources.resource_filename('authors_clasiffier', '/data/Love of lesbian.txt') 
+ae_3= pkg_resources.resource_filename('authors_clasiffier', '/data/jose_jose.txt') 
+ae_4= pkg_resources.resource_filename('authors_clasiffier', '/data/luis_miguel.txt') 
+ae_5= pkg_resources.resource_filename('authors_clasiffier', '/data/melendi.txt') 
+ae_6= pkg_resources.resource_filename('authors_clasiffier', '/data/miguel_bose.txt') 
+ae_t = pkg_resources.resource_filename('authors_clasiffier', '/data/autores_totales.txt') 
