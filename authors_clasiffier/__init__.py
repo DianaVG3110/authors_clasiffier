f2bdf2d4e4fd42981b93b5eb142873377767bd6d
Iván García <77176510+ivanGarciaBernal@users.noreply.github.com>
2024-01-09 22:38:25 -0600

    Update __init__.py

--- authors_clasiffier/__init__.py
+++ authors_clasiffier/__init__.py
@@ -1,4 +1,4 @@
 from authors_clasiffier.classify import classify
-import authors_clasiffier.resources
+from authors_clasiffier.resources import *
 from authors_clasiffier.word_cloud import gen_wordcloud
 from authors_clasiffier.sentiment_analysis import analysis
